3a094ab87c455bde7959dd56c1948a047ec29a16
ChaseyWasey27 <cjnpgachamp@gmail.com>
2026-07-24 08:12:19 -1000

    Add challenge 5

---
+++ Challenges/Challenge_5/Encrypt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+static void transform(char *buf, size_t len)
+{
+    for (size_t i = 0; i < len; i++) {
+        char c = buf[i];
+        if (c >= 'a' && c <= 'z')
+            buf[i] = (char)(((c - 'a' + 13) % 26) + 'a');
+        else if (c >= 'A' && c <= 'Z')
+            buf[i] = (char)(((c - 'A' + 13) % 26) + 'A');
+        /* digits, '_' and '!' pass through untouched */
+    }
+}
+
+int main(void)
+{
+    char flag[] = "1nv35t1g4t1v3_R3v3rs3r!";
+    size_t n = strlen(flag);
+
+    transform(flag, n);
+    printf("%s\n", flag);   /* also its own decryptor - run twice */
+    return 0;
+}
